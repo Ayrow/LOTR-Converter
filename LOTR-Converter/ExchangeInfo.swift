4a054f78b93e715dedd43957da80fb740cb92d72
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-12 13:24:19 +0100

    finished design for exchange rate view and added dismiss property

--- LOTR-Converter/ExchangeInfo.swift
+++ LOTR-Converter/ExchangeInfo.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ExchangeInfo: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
         ZStack {
-            // Background image
             Image("parchment")
                 .resizable()
-                .scaledToFill()
                 .ignoresSafeArea()
                 .background(.brown)
             
@@ -25,6 +25,7 @@ struct ExchangeInfo: View {
                 // Info Text
                 Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
                 .font(.title2)
+                .padding()
                 
                 ExchangeRate(leftImage: "goldpiece", text: "1 Gold Piece = 4 Gold Pennies", rightImage: "goldpenny")
                 
@@ -33,10 +34,19 @@ struct ExchangeInfo: View {
                 ExchangeRate(leftImage: "silverpiece", text: "1 Silver Piece = 4 Silver Pennies", rightImage: "silverpenny")
                 
                 ExchangeRate(leftImage: "silverpenny", text: "1 Silver Penny = 100 Copper Pennies", rightImage: "copperpenny")
+            
                 
                 // Button
+                Button("Done") {
+                    dismiss()
+                }
+                .font(.largeTitle)
+                .padding(10)
+                .foregroundColor(.white)
+                .background(.brown)
+                .cornerRadius(15)
             }
-            
+            .foregroundColor(.black)
         }
     }
 }
@@ -44,5 +54,6 @@ struct ExchangeInfo: View {
 struct ExchangeInfo_Previews: PreviewProvider {
     static var previews: some View {
         ExchangeInfo()
+            .preferredColorScheme(.dark)
     }
 }
